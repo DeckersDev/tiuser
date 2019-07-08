39da69d1dc5235bd6ecad93cc830476adbae2e85
griffin.tighe <griffin.tighe@GL4415.corporate.deckers.com>
2019-07-08 11:23:26 -0700

    initial

---
+++ tiusers/src/main/java/com/deckers/tiusers/TiusersApplication.java
@@ -0,0 +1,13 @@
+package com.deckers.tiusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiusersApplication.class, args);
+	}
+
+}
